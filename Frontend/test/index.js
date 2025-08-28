8e567ed7d54e8d565d9609f50078d5caec3150ab
magnitopic <magnitopic@gmail.com>
2025-08-29 00:59:42 +0200

    chore: removed unnecessary files

--- Frontend/test/index.js
+++
@@ -1,9 +0,0 @@
-/* const glob = require("glob");
-
-console.log("launch /test/index.js:", __dirname);
-const files = glob.sync("src/server#<{(||)}>#*.js");
-files.forEach((file) => {
-    console.log("File: ", file);
-    require("../" + file);
-});
- */
